551d5dfaa76669f30de7bd7bf27a30087b584bae
Piotr Szyperek <szyperek.piotr@o2.pl>
2020-09-16 00:51:18 +0200

    Piotr-Szyperek-kodilla-testing-calculator

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 "Task 2 - Modulo 6.2 -> First unit test - Calculator!" 
 "Task 2 - Modulo 6.2 -> First unit test - Calculator!" 
+"# Piotr-Szyperek-kodilla-testing-calculator" 
---
+++ kodilla-testing/src/calculator/Calculator.java
@@ -0,0 +1,27 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public int getA() {
+       return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
